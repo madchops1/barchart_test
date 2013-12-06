56be155abdff2512715ea5ca45c8d98cf21ef19b
Karl Steltenpohl <karl@webksd.com>
2013-12-06 16:47:56 -0600

    -kjs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,7 @@
-
-
-var BarchartCal = {};
-BarchartCal.init = function() {
-	
-	
-	
-};
-
-
 $(document).ready(function() {
-	
-	BarchartCal.init();
-	
 	$(document).on("focus","input",function(e){
-		$(this).val("");
+		if($(this).val == "please enter a note"){
+			$(this).val("");
+		}
 	});
-	
 });
