10cf4c8b6375e9f7be4f04aa24a01b780394166c
Karl Steltenpohl <karl@webksd.com>
2013-12-06 15:36:55 -0600

    -kjs

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ class calendar_notes {
     $output = "First Day: " . $first_day . " | First Day of Week: " . $first_day_of_week . " | Days in Month: " .$days_in_month . "";
     $output .= "<div id='calendar-wrapper'>";
     $output .= "<h1>".$date_info['month']."</h1>";
-    $output .= "<table id='calendar'>";
+    $output .= "<table id='calendar' cellspacing='0' cellpadding='0'>";
     $output .= "  <tr>";
     
     // -- Header
@@ -94,7 +94,7 @@ class calendar_notes {
       // -- A Day...
       $this_day = mktime(0,0,0,$this->month,$day,$this->year);
       $date = date('m/d/y', $this_day);
-      $output .= "<td><div><span>" . $date . "</span><br><input val='please enter a note'/></div></td>";
+      $output .= "<td><div><span>" . $date . "</span><br><input value='please enter a note'/></div></td>";
       
       // -- Weekly Rows
       if(!($i%7)){
@@ -124,11 +124,11 @@ $db = new db;
 		<style>
 		  #calendar-wrapper{
 		    margin:100px auto;
-		    width:700px;
+		    width:840px;
 		  }
 		  
 		  input{
-		    width:100px;
+		    width:120px;
 		    font-size:12px;
 		    font-family:georgia;
 		    font-style:italic;
