cbb8a29618569643a3d2c0e06004ecd48aa27545
Karl Steltenpohl <karl@webksd.com>
2013-12-06 16:28:57 -0600

    -kjs

--- includes/db.class.php
+++ includes/db.class.php
@@ -2,17 +2,17 @@
 // --  Data Layer
 class db {
 
-  var $db_host                     = 'localhost';
-  var $db_name                     = 'barchart';
-  var $db_user                     = 'barchart';
-  var $db_pass                     = 'Barchart1';
+  var $db_host = 'localhost';
+  var $db_name = 'barchart';
+  var $db_user = 'barchart';
+  var $db_pass = 'Barchart1';
 
   function __construct() {
     $this->db_connect();
   }
 
   function db_connect() {
-    // Connect to the Database
+    // -- Connect to the Database
     mysql_connect($this->db_host, $this->db_user, $this->db_pass);
     mysql_select_db($this->db_name);
     unset($this->db_pass);
@@ -22,7 +22,7 @@ class db {
   function db_query($query) {
     $queried = mysql_query($query);
     if(mysql_error()){
-      echo mysql_error();
+      echo mysql_error()."<Br>";
     }
     return $queried;
   }
